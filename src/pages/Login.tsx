f5395ebeb035f3090511be2ad47b133a40078d2a
Javier <javierdiazt406@gmail.com>
2023-04-14 03:16:52 -0400

    CSS refactor and login finish

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import image from "../assets/images/image.png";
 import axios from 'axios';
+import { Notification } from "@mantine/core";
 import { useHistory } from "react-router-dom";
 import "../assets/css/login.css"
 
@@ -9,6 +10,8 @@ type Props = {};
 export default function Login({}: Props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [notification, setNotification] = useState(false);
+  const [error, setError] = useState('');
   
   const history = useHistory();
 
@@ -19,46 +22,65 @@ export default function Login({}: Props) {
   }, []);
 
   const handleLogin = () => {
-    axios.get(`https://sysprop-production.up.railway.app/usuarios?username=${username}&password=${password}`)
+    axios.post(`https://sysprop-production.up.railway.app/usuarios/login`, {
+      username,
+      password
+    })
     .then(() => {
       localStorage.setItem('user', JSON.stringify({ username, password }));
       history.push('/');
-    }).catch(() => {
-      alert('Usuario o contraseña incorrectos');
+      setError('')
+    }).catch((err) => {
+      setNotification(true);
+      setError(err.response.data.message);
+      setTimeout(() => {
+        setNotification(false);
+      }, 3000);
     });
   }   
 
   return (
-    <div className="login-container">
-      <div className="form-body">
-        <img src={image} alt="user-login" />
-        <p className="text">Bienvenido a SysProp</p>
+    <>
+    {
+      notification && (
+        <div className="notification">
+          <Notification color="red" title="Error al intentar loguearse">
+            {error}
+          </Notification>
+        </div>
+      )
+    }
+      <div className="login-container">
+        <div className="form-body">
+          <img src={image} alt="user-login" />
+          <p className="text">Bienvenido a SysProp</p>
 
-        <form className="login-form">
-          <input
-            name="username"
-            type="text"
-            placeholder="Nombre de usuario"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Contraseña"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button 
-            type="button" 
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin();
-            }}
-          >
-            Iniciar Sesión
-          </button>
-          <button type="button">Recuperar Contraseña</button>
-        </form>
+          <form className="login-form">
+            <input
+              name="username"
+              type="text"
+              placeholder="Nombre de usuario"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Contraseña"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button 
+              type="button" 
+              onClick={(e) => {
+                e.preventDefault();
+                handleLogin();
+              }}
+            >
+              Iniciar Sesión
+            </button>
+            <button type="button">Recuperar Contraseña</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
